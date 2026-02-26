be2736bf71b3ef4f8c3fbc8b527a0de5364f3b70
acordero101 <acordero@exeter.edu>
2026-02-26 09:44:18 -0500

    Update Deck.java

--- Deck.java
+++ Deck.java
@@ -1,4 +1,16 @@
+import java.util.ArrayList;
+
 class Deck {
+  ArrayList<String> deck = new ArrayList<String>();
   public Deck() {
-    
+    String[] ranks = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+    String[] suits = ["♤", "♥", "♢", "♧"];
+  }
+
+  public void ge
+
+  public void shuffle() {
+  }
+
+  public 
 }
