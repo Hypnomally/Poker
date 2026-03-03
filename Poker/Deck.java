d9e6dfb82201c76c619a16e16648135529977dc9
Alejandro Cordero <acordero@exeter.edu>
2026-03-03 09:08:10 -0500

    changeshj

--- Poker/Deck.java
+++ Poker/Deck.java
@@ -5,41 +5,46 @@ import java.util.Random;
 class Deck {
 
     ArrayList<String> deck = new ArrayList<String>();
+    public static String[] ranks = {"A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"};
+    public static String[] suits = {"♤", "♥", "♢", "♧"};
   
     public Deck() {
-        public static String[] ranks = {"A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"};
-        public static String[] suits = {"♤", "♥", "♢", "♧"};
-        generateDeck(ranks, suits);
+        deck = generateDeck();
     }
 
-    public static ArrayList<String> generateDeck(String[] ranks, String[] suits) {
+    public static void main(String[] args) {
+        Deck test = new Deck();
+        test.shuffle();
+        System.out.println(test.deck);
+    }
+
+    public static ArrayList<String> generateDeck() {
         ArrayList<String> d = new ArrayList<String>();
         for (String r : ranks) {
             for (String s : suits) {
                 d.add(r + s);
             }
         }
-        System.out.println(d);
         return d;
     }
 
     public void shuffle() {
         Random random = new Random();
         ArrayList<String> e = new ArrayList<String>();
-        int size = this.size();
+        int size = this.deck.size();
         for (int i = 0; i < size; i++) {
-            int a = random.nextInt(0,d.size());
-            e.add(d.get(a));
-            deck.remove(a);
+            int a = random.nextInt(0,this.deck.size());
+            e.add(this.deck.get(a));
+            this.deck.remove(a);
         }
-        d = e;
+        this.deck = e;
     }
 
-    public void dealCards(){
-        for (int i = 0; i < 2; i++) {
-            for (Player p : players) {
-                p.hand[i] = new Card(this);
-            }
-        }
-    }
+    // public void dealCards(){
+    //     for (int i = 0; i < 2; i++) {
+    //         for (Player p : players) {
+    //             p.hand[i] = new Card(this);
+    //         }
+    //     }
+    // }
 }
