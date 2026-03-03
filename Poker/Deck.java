dc0b6317266ece7837ab914638a0308dbfec141c
Hypnomally <115428118+Hypnomally@users.noreply.github.com>
2026-03-03 10:52:03 -0500

    sdf

--- Poker/Deck.java
+++ Poker/Deck.java
@@ -2,11 +2,12 @@ package Poker;
 import java.util.ArrayList;
 import java.util.Random;
 
+
 class Deck {
 
     ArrayList<String> deck = new ArrayList<String>();
     public static String[] ranks = {"2:", "3:", "4:", "5:", "6:", "7:", "8:", "9:", "10:", "J:", "Q:", "K:", "A:"};
-    public static String[] suits = {"♤", "♥", "♢", "♧"};
+    public static String[] suits = {"♠", "♥", "♦", "♣"};
   
     public Deck() {
         deck = generateDeck();
