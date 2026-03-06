8873dfc594bf2c00699c9892f764b0ac9ff21b71
Hypnomally <115428118+Hypnomally@users.noreply.github.com>
2026-03-06 17:32:47 -0500

    changes

--- src/HandStrength.java
+++ src/HandStrength.java
@@ -1,6 +1,15 @@
-/*package Poker;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 
 public class HandStrength {
+
+    public enum HandRank{
+        High_Card(1), Pair(2), Two_Pair(3), Three_of_a_kind(4), Straight(5), Flush(6), Full_House(7), Four_Of_A_Kind(8), Straight_Flush(9), Royal_Flush(10);
+
+        private final int value;
+        HandRank(int value) {this.value = value;}
+        public int getValue() {return value;}
+    }
     
 }
-*/
--- src/Player.java
+++ src/Player.java
@@ -5,9 +5,35 @@ public class Player{
     private int chips;
     private ArrayList<Card> hand  = new ArrayList<>();
     public boolean isFolded = false;
+    private int currentBet = 0;
 
     public Player(String name, int chips) {
         this.name = name;
         this.chips = chips;
     }
+
+    public void addCard(Card card){
+
+    }
+
+    public void bet(int amount){
+        if (amount < chips) {
+            this.chips -= amount;
+            this.currentBet += amount;
+        }
+    }
+
+    public void fold(){
+        this.isFolded = true;
+    }
+
+    public void clearHand(){
+        hand.clear();
+        isFolded = false;
+        currentBet = 0;
+    }
+
+    public ArrayList<Card> gethand() { return hand;}
+    public int getChips() { return chips;}
+    public String getName() { return name;}
 }
