ab3982ab6602c0488263755becac52396dcfa9bb
Hypnomally <115428118+Hypnomally@users.noreply.github.com>
2026-03-07 01:04:24 -0500

    g

--- src/PokerBot.java
+++ src/PokerBot.java
@@ -3,13 +3,29 @@ import java.util.ArrayList;
 public class PokerBot {
     private HandStrength evaluator = new HandStrength();
 
-    Public String getBestMove(GameState currentState){
+    public String getBestMove(GameState currentState){
         //Create root of our tree
         PokerNode root = new PokerNode(currentState);
 
         buildTree(root,2);
 
-        return selectBestAction(root;)
+        return selectBestAction(root);
+    }
+
+    private String selectBestAction(PokerNode root){
+        String bestMove = "fold";
+        int maxScore = -9999;
+
+        for(PokerNode child : root.getChildren()) {
+            if (child.getValue() > maxScore){
+                maxScore = child.getValue();
+                bestMove = child.getMoveApplied();
+            }
+        }
+
+        if (maxScore < 15) return "fold";
+
+        return bestMove;
     }
 
     private void buildTree(PokerNode node, int depth){
@@ -24,7 +40,7 @@ public class PokerBot {
             simulatedState.applyMove(move,50);
 
             PokerNode child = new PokerNode(simulatedState);
-            child.seetMoveApplied(move);
+            child.setMoveApplied(move);
 
             int score = evaluator.calculateTotalStrength(
                 simulatedState.getPlayers().get(1).gethand(), 
---
+++ src/PokerNode.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class PokerNode {
+    private GameState state;
+    private List<PokerNode> children = new ArrayList<>();
+    private int value;
+    private String moveApplied;
+
+    public PokerNode(GameState state){
+        this.state = state;
+    }
+
+    public void addChild(PokerNode child) {children.add(child);}
+    public GameState getState() { return state;}
+    public int getValue() {return value;}
+    public void setValue(int v) {this.value = v;}
+    public String getMoveApplied() {return moveApplied;}
+    public void setMoveApplied(String m) {this.moveApplied = m;}
+    public List<PokerNode> getChildren() {return children;}
+}
